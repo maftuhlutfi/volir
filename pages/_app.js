b3192c089fcb819dbf8f49fe9e2cdced5ac93b7c
maftuhlutfi <playmakernyomlo@gmail.com>
2022-02-23 11:32:47 +0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import NProgress from 'nprogress';
+import { Router } from 'next/router';
+
 import '../styles/globals.css'
 import '../styles/fonts.css'
+import '../styles/nprogress.css'
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
