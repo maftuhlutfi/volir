a96780ae4e0e66f014e7f91719bd9d233b54233a
maftuhlutfi <playmakernyomlo@gmail.com>
2022-02-27 11:30:41 +0700

    fix html for and id

--- components/shared/Input/TextField.js
+++ components/shared/Input/TextField.js
@@ -1,9 +1,9 @@
 const TextField = ({ label, id, className, ...otherProps }) => {
     return (
         <div className={`flex flex-col ${className}`}>
-            <label for={id} className='mb-2 font-semibold'>{label}</label>
-            <input className="p-4 border border-gray-300 rounded-lg outline-none focus:ring-1 ring-emerald-400" {...otherProps} />
-        </div>
+            <label htmlFor={id} className='mb-2 font-semibold'>{label}</label>
+            <input id={id} className="p-4 border border-gray-300 rounded-lg outline-none focus:ring-1 ring-emerald-400" {...otherProps} />
+        </div >
     );
 }
 
