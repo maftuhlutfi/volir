d1a8221eb3251ec92bdef30765eebc957da65412
maftuhlutfi <playmakernyomlo@gmail.com>
2022-02-27 11:31:02 +0700

    add className props

--- components/shared/Card/index.js
+++ components/shared/Card/index.js
@@ -1,6 +1,6 @@
-const Card = ({ children, className }) => {
+const Card = ({ children, className, ...otherProps }) => {
     return (
-        <div className={`rounded-[20px] bg-white shadow-md border border-gray-300 ${className.includes('p-') ? className : 'p-8'}`}>
+        <div className={`rounded-[20px] bg-white shadow-md border border-gray-300 ${className.includes('p-') ? className : 'p-8'} ${className}`} {...otherProps}>
             {children}
         </div>
     );
